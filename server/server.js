4c7f740002d295fe81c4ca94c43bb33e45aa2528
nhule90 <lehoanhu90@gmail.com>
2023-07-27 00:33:31 -0500

    test new layout

--- server/server.js
+++ server/server.js
@@ -1,18 +1,12 @@
 const express = require('express');
-// const cors = require("cors");
+const path = require('path')
 const app = express()
 
 app.use(express.json())  // When we want to be able to accept JSON.
-// app.use(cors());
 app.use(express.static(`${__dirname}/public`))
-// const { getHouses, createHouse, deleteHouse, updateHouse } = require('./controller.js')
-
-// app.get('/api/houses', getHouses)
-
-// app.post('/api/houses', createHouse)
-
-// app.delete('/api/houses/:id', deleteHouse)
-
-// app.put('/api/houses/:id', updateHouse)
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 app.listen(4000, () => console.log('Server running on 4000'))
+
